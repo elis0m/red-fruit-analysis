c0a57a3e531f59bd29922684dd71a738487a67eb
elis0m <sominzzang615@gmail.com>
2020-10-05 18:57:24 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Red fruit analysis
+_붉은 과일 분석 서비스_
+
+<빅데이터 분석> 머신러닝 서비스 실습
+
+[머신러닝 서비스 바로가기](https://elis0m.github.io/red-fruit-analysis/)
---
+++ dataCollection/0929ML(1).py
@@ -0,0 +1,5 @@
+from bing_image_downloader import downloader
+
+downloader.download("one cherry", limit=20,  output_dir="dataset")
+downloader.download("one blueberry", limit=20,  output_dir="dataset")
+downloader.download("one raspberry", limit=20,  output_dir="dataset")
